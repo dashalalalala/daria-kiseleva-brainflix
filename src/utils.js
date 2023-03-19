3fcafff6cb460cdd04cea546a6af2054696fb85d
dashalalalala <dchernikaa@gmail.com>
2023-03-18 17:25:21 -0700

    updated API url

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-
 //API Data
 export const apiKey = "01dc537b-ea56-461e-a1aa-6e5b676f6493";
 
-export const apiUrl = "https://project-2-api.herokuapp.com/videos";
+export const apiUrl = "http://localhost:8080/videos/";
